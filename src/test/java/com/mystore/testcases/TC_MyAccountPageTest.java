a11d430caf87efc400610e4dc90970b7cf645616
NikhilSatpute <NikhilSatpute@DESKTOP-21G44T2>
2024-05-20 16:10:11 +0530

    BaseClass changed

--- src/test/java/com/mystore/testcases/TC_MyAccountPageTest.java
+++ src/test/java/com/mystore/testcases/TC_MyAccountPageTest.java
@@ -2,11 +2,10 @@ package com.mystore.testcases;
 
 public class TC_MyAccountPageTest extends BaseClass {
 
-public static void main(String[] args) {
-	
-System.out.println();	
-	
-}	
-	
-	
+	public static void main(String[] args) {
+
+		System.out.println();
+
+	}
+
 }
